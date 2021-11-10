f19c73a5cbd33cc9d30e14026a85dca06d75e696
Michał Łaszczewski <michal@laszczewski.com>
2021-11-10 11:36:46 +0100

    uid generator access, vue-api update

--- index.js
+++ index.js
@@ -3,33 +3,38 @@ import { live as d3live } from '@live-change/dao-vue3'
 
 function path(app) {
   app = app || getCurrentInstance()
-  return app.appContext.config.globalProperties.$fetch
+  return app.appContext.config.globalProperties.$lc.fetch
 }
 
 function api(app) {
   app = app || getCurrentInstance()
-  return app.appContext.config.globalProperties.$api
+  return app.appContext.config.globalProperties.$lc
 }
 
 function view(app) {
   app = app || getCurrentInstance()
-  return app.appContext.config.globalProperties.$view
+  return app.appContext.config.globalProperties.$lc.view
 }
 
 function actions(app) {
   app = app || getCurrentInstance()
-  return app.appContext.config.globalProperties.$actions
+  return app.appContext.config.globalProperties.$lc.actions
 }
 
 function live(path) {
   const app = getCurrentInstance()
-  const api = app.appContext.config.globalProperties.$api
+  const api = app.appContext.config.globalProperties.$lc
   return d3live(api, path)
 }
 
 function client(app) {
   app = app || getCurrentInstance()
-  return app.appContext.config.globalProperties.$client
+  return app.appContext.config.globalProperties.$lc.client
 }
 
-export { path, api, view, actions, live, client }
+function uid(app) {
+  app = app || getCurrentInstance()
+  return app.appContext.config.globalProperties.$lc.uid
+}
+
+export { path, api, view, actions, live, client, uid }
